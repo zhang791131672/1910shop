d648f6d9689be3ecc247def22f2a02d37811655b
zhangbowen <791131672@qq.com>
2020-06-15 16:20:45 +0800

    测试

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                   1910 Laravel
                 </div>
 
                 <div class="links">
