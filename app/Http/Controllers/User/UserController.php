0b5077e324c46274616142cfa353040c23483be3
zhangbowen <791131672@qq.com>
2020-06-20 09:31:42 +0800

    测试用户注册

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -7,5 +7,18 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    //用户注册
+    public function reg(){
+        $name=$_POST['name'];
+        $email=$_POST['email'];
+        $pass1=$_POST['pass1'];
+        $pass2=$_POST['pass2'];
+        $arr=[
+            'name'=>$name,
+            'email'=>$email,
+            'pass1'=>$pass1,
+            'pass2'=>$pass2
+        ];
+        return json_encode($arr);
+    }
 }
